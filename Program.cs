f23d2562440d88abe0fcc166223ac5d98fdaee1e
baal98 <98423456+baal98@users.noreply.github.com>
2022-03-21 21:19:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Rage_Quit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Regex regex = new Regex(@"(?'chars'[^\d]+)(?'digits'\d+)");
+
+            MatchCollection matches = regex.Matches(command);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Match match in matches)
+            {
+                string chars = match.Groups["chars"].Value.ToString().ToUpper();
+                int digits = int.Parse(match.Groups["digits"].Value);
+                for (int i = 0; i < digits; i++)
+                {
+                    sb.Append(chars);
+                }
+            }
+            char[] charsToCheck = sb.ToString().ToCharArray();
+            List<char> listChars = new List<char>();
+
+            foreach (char c in charsToCheck)
+            {
+                if (!listChars.Contains(c))
+                {
+                    listChars.Add(c);
+                }
+            }
+            int counter = listChars.Count;
+
+            Console.WriteLine($"Unique symbols used: {counter}");
+            Console.WriteLine(sb);
+        }
+
+    }
+}
